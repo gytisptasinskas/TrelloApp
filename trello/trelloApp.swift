7ba0719309f9988fe8ca14c87d5ec37c78de5901
Gytis Ptasinskas <ptasinskasgytis@gmail.com>
2024-02-26 15:04:52 +0200

    Initial commit

---
+++ trello/trelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  trelloApp.swift
+//  trello
+//
+//  Created by Gytis Ptasinskas on 26/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct trelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
